e78b0ca89c1971b8b384732b4a4b8d54351c8cbb
hotcocoadotcom <77078601+hotcocoadotcom@users.noreply.github.com>
2026-08-11 16:30:50 -0400

    Initial commit

---
+++ PicoGLITCHER.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/gpio.h"
+
+const uint POST_BASE = 0;
+const uint32_t POST_MASK = 0x000000FF;
+
+const uint CPU_PLL_BYPASS = 8;
+const uint CPU_RST = 9;
+
+void pin_setup() {
+    //FOR USE WITH 1N4148 DIODE OR 8-CHANNEL LEVEL SHIFTER. WIRE PINS 0-7 TO BITS 0-7 ON 360 MOTHERBOARD.
+    for (uint pin = POST_BASE; pin <= 7; pin++) {
+        gpio_init(pin);
+        gpio_set_dir(pin, GPIO_IN);
+        gpio_pull_up(pin);
+    }
+
+    //FOR USE WITH 22k OHM RESISTOR OR 3-10k OHM RESISTOR DEPENDING ON MOTHERBOARD TYPE.
+    gpio_init(CPU_PLL_BYPASS);
+    gpio_set_dir(CPU_PLL_BYPASS, GPIO_OUT);
+
+    gpio_init(CPU_RST);
+    gpio_set_dir(CPU_RST, GPIO_OUT);
+}
+
+int main() {
+    stdio_init_all();
+    pin_setup();
+
+    uint8_t last_code = 0xFF;
+
+    while (true) {
+        uint8_t current_code = (uint8_t)(gpio_get_all() & POST_MASK);
+        if (current_code != last_code) {
+            printf("%02X\n", current_code);
+            last_code = current_code;
+        }
+        
+        sleep_us(1);
+    }
+
+
+}
