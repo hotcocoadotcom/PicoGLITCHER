0c3eaf7b4f8fc96d28c4ef84086701440eb41d8b
cocoa <77078601+hotcocoadotcom@users.noreply.github.com>
2026-09-15 08:35:29 -0400

    edarkj;lg

--- PicoGLITCHER.c
+++ PicoGLITCHER.c
@@ -3,15 +3,9 @@
 #include "hardware/pio.h"
 #include "hardware/gpio.h"
 
-const uint POST_BASE = 0;
-const uint32_t POST_MASK = 0x000000FF;
-
-const uint CPU_PLL_BYPASS = 8;
-const uint CPU_RST = 9;
-
 void pin_setup() {
     //For use with 1N4148 diode or 8 channel level shifter. Wire GPIO 0-7 to bits 0-7 on the motherboard.
-    for (uint pin = POST_BASE; pin <= 7; pin++) {
+    for (uint pin = 0; pin <= 7; pin++) {
         gpio_init(pin);
         gpio_set_dir(pin, GPIO_IN);
         gpio_pull_up(pin);
@@ -25,7 +19,7 @@ int main() {
     uint8_t last_code = 0xFF;
 
     while (true) {
-        uint8_t current_code = (uint8_t)(gpio_get_all() & POST_MASK);
+        uint8_t current_code = (uint8_t)(gpio_get_all() & 0x000000FF);
         if (current_code != last_code) {
             printf("%02X\n", current_code);
             last_code = current_code;
